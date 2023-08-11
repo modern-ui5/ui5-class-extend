5dd734aedc4a88fd75a5901527cbd76e5d91b1e5
Yichuan Shen <shen.yichuan@gmail.com>
2023-08-11 22:05:48 +0200

    Add MetadataToSettings

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import Button from "sap/m/Button";
 import Event from "sap/ui/base/Event";
-import ManagedObject, { MetadataOptions } from "sap/ui/base/ManagedObject";
+import ManagedObject, {
+  AggregationBindingInfo,
+  MetadataOptions,
+  PropertyBindingInfo,
+} from "sap/ui/base/ManagedObject";
 import Control from "sap/ui/core/Control";
 
 declare const typeTag: unique symbol;
@@ -14,6 +17,8 @@ type TypeStringToType<T extends string, F = any> = T extends "string"
   ? string
   : T extends "int" | "float"
   ? number
+  : T extends "boolean"
+  ? boolean
   : T extends "object"
   ? object
   : T extends "function"
@@ -24,14 +29,13 @@ type TypeStringToType<T extends string, F = any> = T extends "string"
 
 type MetadataItemToType<T, F = any> = typeof typeTag extends keyof T
   ? T[typeof typeTag]
-  : TypeStringToType<
-      T extends string
-        ? T
-        : "type" extends keyof T
-        ? T["type"] & string
-        : "any",
-      F
-    >;
+  : T extends string
+  ? TypeStringToType<T, F> | undefined
+  : "type" extends keyof T
+  ?
+      | TypeStringToType<T["type"] & string, F>
+      | ("defaultValue" extends keyof T ? T["defaultValue"] : undefined)
+  : F;
 
 type MetadataItemVisible<T> = "visibility" extends keyof T
   ? T["visibility"] extends "hidden"
@@ -39,120 +43,117 @@ type MetadataItemVisible<T> = "visibility" extends keyof T
     : true
   : true;
 
-type PropertiesToMethods<This, T> = {
-  [K in keyof T & string as `get${Capitalize<K>}`]: (
-    this: This
-  ) => MetadataItemToType<T[K]>;
+type PropertiesToMethods<T> = {
+  [K in keyof T & string as `get${Capitalize<K>}`]: () => MetadataItemToType<
+    T[K]
+  >;
 } & {
   [K in keyof T & string as `set${Capitalize<K>}`]: (
-    this: This,
     value: MetadataItemToType<T[K]>
-  ) => This;
+  ) => void;
 };
 
-type SingleAggregationsToMethods<This, T> = {
-  [K in keyof T & string as `get${Capitalize<K>}`]: (
-    this: This
-  ) => MetadataItemToType<T[K], ManagedObject> | null;
+type SingleAggregationsToMethods<T> = {
+  [K in keyof T & string as `get${Capitalize<K>}`]: () => MetadataItemToType<
+    T[K],
+    ManagedObject
+  > | null;
 } & {
   [K in keyof T & string as `set${Capitalize<K>}`]: (
-    this: This,
     value: MetadataItemToType<T[K], ManagedObject> | null
-  ) => This;
+  ) => void;
 } & {
-  [K in keyof T & string as `destroy${Capitalize<K>}`]: (this: This) => This;
+  [K in keyof T & string as `destroy${Capitalize<K>}`]: () => void;
 };
 
 type SingularName<N, T> = "singularName" extends keyof T
   ? T["singularName"] & string
   : N;
 
-type MultipleAggregationsToMethods<This, T> = {
-  [K in keyof T & string as `get${Capitalize<K>}`]: (
-    this: This
-  ) => MetadataItemToType<T[K], ManagedObject>[];
+type MultipleAggregationsToMethods<T> = {
+  [K in keyof T & string as `get${Capitalize<K>}`]: () => MetadataItemToType<
+    T[K],
+    ManagedObject
+  >[];
 } & {
-  [K in keyof T & string as `removeAll${Capitalize<K>}`]: (
-    this: This
-  ) => MetadataItemToType<T[K], ManagedObject>[];
+  [K in keyof T &
+    string as `removeAll${Capitalize<K>}`]: () => MetadataItemToType<
+    T[K],
+    ManagedObject
+  >[];
 } & {
-  [K in keyof T & string as `destroy${Capitalize<K>}`]: (this: This) => This;
+  [K in keyof T & string as `destroy${Capitalize<K>}`]: () => void;
 } & {
   [K in keyof T & string as `add${Capitalize<SingularName<K, T[K]>>}`]: (
-    this: This,
     item: MetadataItemToType<T[K], ManagedObject>
-  ) => This;
+  ) => void;
 } & {
   [K in keyof T & string as `remove${Capitalize<SingularName<K, T[K]>>}`]: (
-    this: This,
     item: number | string | MetadataItemToType<T[K], ManagedObject>
   ) => MetadataItemToType<T[K], ManagedObject> | null;
 } & {
   [K in keyof T & string as `insert${Capitalize<SingularName<K, T[K]>>}`]: (
-    this: This,
     item: MetadataItemToType<T[K], ManagedObject>,
     index: number
-  ) => This;
+  ) => void;
 } & {
   [K in keyof T & string as `indexOf${Capitalize<SingularName<K, T[K]>>}`]: (
-    this: This,
     item: MetadataItemToType<T[K], ManagedObject>
   ) => number;
 };
 
-type SingleAssociationsToMethods<This, T> = {
-  [K in keyof T & string as `get${Capitalize<K>}`]: (
-    this: This
-  ) => string | null;
+type SingleAssociationsToMethods<T> = {
+  [K in keyof T & string as `get${Capitalize<K>}`]: () => string | null;
 } & {
   [K in keyof T & string as `set${Capitalize<K>}`]: (
-    this: This,
     value: string | MetadataItemToType<T[K], ManagedObject> | null
-  ) => This;
+  ) => void;
 };
 
-type MultipleAssociationsToMethods<This, T> = {
-  [K in keyof T & string as `get${Capitalize<K>}`]: (this: This) => string[];
+type MultipleAssociationsToMethods<T> = {
+  [K in keyof T & string as `get${Capitalize<K>}`]: () => string[];
 } & {
-  [K in keyof T & string as `removeAll${Capitalize<K>}`]: (
-    this: This
-  ) => string[];
+  [K in keyof T & string as `removeAll${Capitalize<K>}`]: () => string[];
 } & {
   [K in keyof T & string as `add${Capitalize<SingularName<K, T[K]>>}`]: (
-    this: This,
     item: string | MetadataItemToType<T[K], ManagedObject>
-  ) => This;
+  ) => void;
 } & {
   [K in keyof T & string as `remove${Capitalize<SingularName<K, T[K]>>}`]: (
-    this: This,
     item: MetadataItemToType<T[K], ManagedObject>
   ) => number | string | MetadataItemToType<T[K], ManagedObject> | null;
 };
 
-type EventsToMethods<This, T> = {
+type EventParameters<T> = "parameters" extends keyof T
+  ? {
+      [K in keyof T["parameters"]]: MetadataItemToType<T["parameters"][K]>;
+    }
+  : {};
+
+type EventsToMethods<T> = {
   [K in keyof T & string as `attach${Capitalize<K>}`]: {
     (
-      this: This,
       data: object,
-      handler: (evt: Event) => void,
+      handler: (evt: Event<EventParameters<T[K]>>) => void,
       listener?: object
-    ): This;
-    (this: This, handler: (evt: Event) => void, listener?: object): This;
+    ): void;
+    (
+      handler: (evt: Event<EventParameters<T[K]>>) => void,
+      listener?: object
+    ): void;
   };
 } & {
   [K in keyof T & string as `detach${Capitalize<K>}`]: (
-    this: This,
     handler: Function,
     listener?: object
-  ) => This;
+  ) => void;
 } & {
   [K in keyof T & string as `fire${Capitalize<K>}`]: {
-    (this: This, parameters?: object): This;
+    (parameters?: object): void;
   };
 };
 
-type MetadataToInterface<This, M extends MetadataOptions> = PropertiesToMethods<
-  This,
+type MetadataToInterface<M extends MetadataOptions> = PropertiesToMethods<
   OmitNever<{
     [K in keyof M["properties"]]: MetadataItemVisible<
       M["properties"][K]
@@ -164,79 +165,135 @@ type MetadataToInterface<This, M extends MetadataOptions> = PropertiesToMethods<
   }>
 > &
   SingleAggregationsToMethods<
-    This,
     OmitNever<{
       [K in keyof M["aggregations"]]: MetadataItemVisible<
         M["aggregations"][K]
       > extends false
         ? never
-        : M["aggregations"][K] extends MetadataOptions.Aggregation
-        ? M["aggregations"][K]["multiple"] extends false
-          ? M["aggregations"][K]
-          : never
+        : M["aggregations"][K] extends MetadataOptions.Aggregation & {
+            multiple: false;
+          }
+        ? M["aggregations"][K]
         : never;
     }>
   > &
   MultipleAggregationsToMethods<
-    This,
     OmitNever<{
       [K in keyof M["aggregations"]]: MetadataItemVisible<
         M["aggregations"][K]
       > extends false
         ? never
-        : M["aggregations"][K] extends MetadataOptions.Aggregation
-        ? M["aggregations"][K]["multiple"] extends false
-          ? never
-          : M["aggregations"][K]
+        : M["aggregations"][K] extends MetadataOptions.Aggregation & {
+            multiple: false;
+          }
+        ? never
         : M["aggregations"][K];
     }>
   > &
   SingleAssociationsToMethods<
-    This,
     OmitNever<{
       [K in keyof M["associations"]]: MetadataItemVisible<
         M["associations"][K]
       > extends false
         ? never
-        : M["associations"][K] extends MetadataOptions.Association
-        ? M["associations"][K]["multiple"] extends false
-          ? M["associations"][K]
-          : never
+        : M["associations"][K] extends MetadataOptions.Association & {
+            multiple: false;
+          }
+        ? M["associations"][K]
         : never;
     }>
   > &
   MultipleAssociationsToMethods<
-    This,
     OmitNever<{
       [K in keyof M["associations"]]: MetadataItemVisible<
         M["associations"][K]
       > extends false
         ? never
-        : M["associations"][K] extends MetadataOptions.Association
-        ? M["associations"][K]["multiple"] extends false
-          ? never
-          : M["associations"][K]
+        : M["associations"][K] extends MetadataOptions.Association & {
+            multiple: false;
+          }
+        ? never
         : M["associations"][K];
     }>
   > &
-  EventsToMethods<This, M["events"]>;
+  EventsToMethods<M["events"]>;
+
+type MetadataToSettings<T, M extends MetadataOptions> = Partial<
+  // Properties
+  OmitNever<{
+    [K in keyof M["properties"]]: MetadataItemVisible<
+      M["properties"][K]
+    > extends false
+      ? never
+      :
+          | MetadataItemToType<M["properties"][K], unknown>
+          | PropertyBindingInfo
+          | `{${string}}`;
+  }> &
+    // Aggregations
+    OmitNever<{
+      [K in keyof M["aggregations"]]: MetadataItemVisible<
+        M["aggregations"][K]
+      > extends false
+        ? never
+        : M["aggregations"][K] extends MetadataOptions.Aggregation & {
+            multiple: false;
+          }
+        ? MetadataItemToType<M["aggregations"][K], unknown>
+        :
+            | MetadataItemToType<M["aggregations"][K], unknown>
+            | MetadataItemToType<M["aggregations"][K], unknown>[]
+            | AggregationBindingInfo
+            | `{${string}}`;
+    }> &
+    // Associations
+    OmitNever<{
+      [K in keyof M["associations"]]: MetadataItemVisible<
+        M["associations"][K]
+      > extends false
+        ? never
+        : M["associations"][K] extends MetadataOptions.Association & {
+            multiple: false;
+          }
+        ? MetadataItemToType<M["associations"][K], unknown> | string
+        : (MetadataItemToType<M["associations"][K], unknown> | string)[];
+    }> & {
+      // Events
+      [K in keyof M["events"]]: (
+        evt: Event<EventParameters<M["events"][K]>>
+      ) => void;
+    }
+> &
+  (T extends new (id: string, settings: infer S) => any ? S : {});
 
 export function typed<T>(): <const U>(value: U) => U & { [typeTag]: T } {
   return (value) => value as any;
 }
 
-export function createBaseClass<
+export function ui5Extend<
   T extends new (...args: any) => any,
   const M extends MetadataOptions
->(base: T, metadata: M): <This>() => new () => MetadataToInterface<This, M> {
-  return () => base as any;
+>(
+  base: T,
+  metadata: M
+): {
+  new (settings?: MetadataToSettings<T, M>): InstanceType<T> &
+    MetadataToInterface<M>;
+  new (id: string, settings?: MetadataToSettings<T, M>): InstanceType<T> &
+    MetadataToInterface<M>;
+} {
+  return base as any;
 }
 
-const ControlBase = createBaseClass(Control, {
+class TestClass extends ui5Extend(Control, {
   properties: {
     text: typed<"on" | "off">()({
       type: "string",
+      defaultValue: "on",
     }),
+    active: {
+      type: "boolean",
+    },
   },
   aggregations: {
     button: {
@@ -251,12 +308,25 @@ const ControlBase = createBaseClass(Control, {
       singularName: "selectedItem",
     },
   },
-});
-
-class TestClass extends ControlBase<TestClass>() {
+  events: {
+    click: {
+      parameters: {
+        count: "int",
+        text: "string",
+      },
+    },
+  },
+}) {
   constructor() {
     super();
 
     this.setText("on");
+    this.getText();
+    this.getActive();
+    this.attachClick((evt) => {
+      evt.getParameter("count");
+      evt.getParameter("text");
+    });
+    this.getButton();
   }
 }
